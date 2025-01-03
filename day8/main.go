f53e1db9981da35ffa402d978bc4dae82f98c79b
Jacob Maizel <maizeljacob@gmail.com>
2025-01-02 20:12:24 -0500

    d8p1

---
+++ day8/main.go
@@ -0,0 +1,195 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+/*
+Day 8
+antenna has been reconfigured to emit a signal that makes people 0.1% more likely to ...
+Each antenna is tuned to a specific frequency indicated by a single lowercase letter, uppercase letter, or digit
+
+signal only applies its nefarious effect at specific antinodes based on the resonant frequencies of the antennas
+
+antinode occurs at any point that is perfectly in line with two antennas of the same frequency - but only when one of the antennas is twice as far away as the other
+
+for any pair of antennas with the same frequency, there are two antinodes, one on either side of them
+
+antinodes can occur at locations that contain antennas
+
+How many unique locations within the bounds of the map contain an antinode?
+
+
+keep looping through all
+until we find a non empty spot
+create a new antenna pair with that being the start
+
+keep looping until we find a matching antenna for the one we have
+calculate anti node positions for both
+and consider that pair completed
+
+now if we are looping and
+
+
+loop over all of the items
+if its non empty space,
+add it to a list of its own kind, each with their corresponding positions
+
+ie. saw A at 5,6 8,10, and 10,2
+etc
+
+each of them represent a unique combination
+
+calculate the anti node positions for all of the availabile pairs,
+using a map set to track unique positions where they are found
+
+
+
+*/
+
+type antenna struct {
+	pos        position
+	signalChar byte
+}
+
+func newAntenna(signalChar byte, pos position) antenna {
+	return antenna{pos, signalChar}
+}
+
+func (a *antenna) String() string {
+	return fmt.Sprintf("%s=%s", string(a.signalChar), a.pos.String())
+}
+
+func (a *antennaPair) String() string {
+	return fmt.Sprintf("%s -> %s", a.start.String(), a.end.String())
+}
+
+type position struct {
+	row int
+	col int
+}
+
+type antennaPair struct {
+	start antenna
+	end   antenna
+}
+
+func (p *position) String() string {
+	return fmt.Sprintf("[%d,%d]", p.row, p.col)
+}
+
+func newPos(row, col int) position {
+	return position{row, col}
+}
+
+type input struct {
+	antinodePositions map[position]bool
+	antennaLocations  map[byte][]antenna
+	antennaPairs      []antennaPair
+	grid              [][]byte
+	rows              int
+	cols              int
+}
+
+// distance from start is distance * 2
+// do for both start -> end and end -> start
+// save position in input
+func (i *input) computePart1() {
+	for _, p := range i.antennaPairs {
+		// fmt.Printf("Starting AP bounds for pair=%s\n", p.String())
+		dist1Row, dist1Col := distance(p.start.pos, p.end.pos)
+
+		// fmt.Printf("dist1Row=%d, dist1Col=%d\n", dist1Row, dist1Col)
+
+		// fmt.Printf("api1 = %d+%d*2, %d+%d*2\n", p.start.pos.row, dist1Row, p.start.pos.col, dist1Col)
+		ap1 := newPos(p.start.pos.row+dist1Row*2, p.start.pos.col+dist1Col*2)
+		// fmt.Printf("ap1: %s\n", ap1.String())
+
+		if i.inBounds(&ap1) {
+			// fmt.Println("ap1 was in bounds!")
+			i.antinodePositions[ap1] = true
+		}
+
+		dist2Row, dist2Col := distance(p.end.pos, p.start.pos)
+		ap2 := newPos(p.end.pos.row+dist2Row*2, p.end.pos.col+dist2Col*2)
+		// fmt.Printf("ap2: %s\n", ap2.String())
+		if i.inBounds(&ap2) {
+			// fmt.Println("ap2 was in bounds!")
+			i.antinodePositions[ap2] = true
+		}
+	}
+}
+
+// as part of the initial reading, we can build the combos
+// as well as their reversals at the same time
+func newInput(r io.Reader) *input {
+	i := &input{antinodePositions: make(map[position]bool), antennaLocations: make(map[byte][]antenna)}
+	s := bufio.NewScanner(r)
+	for s.Scan() {
+		rawLine := strings.TrimSpace(s.Text())
+		rawlineBytes := []byte(rawLine)
+		for idx, a := range rawlineBytes {
+			if a == EMPTY_SPACE {
+				continue
+			}
+			x := newAntenna(a, newPos(len(i.grid), idx))
+			i.createPairs(x)
+			i.antennaLocations[a] = append(i.antennaLocations[a], x)
+		}
+		i.grid = append(i.grid, rawlineBytes)
+	}
+	i.rows = len(i.grid) - 1
+	i.cols = len(i.grid[0]) - 1
+	// fmt.Println("max rows=", i.rows, "max cols=", i.cols)
+	return i
+}
+
+func newAntennaPair(start, end antenna) antennaPair {
+	return antennaPair{start, end}
+}
+
+func (i *input) createPairs(newA antenna) []antennaPair {
+	ans := []antennaPair{}
+	list := i.antennaLocations[newA.signalChar]
+	for _, a := range list {
+		p := newAntennaPair(newA, a)
+		ans = append(ans, p)
+		i.antennaPairs = append(i.antennaPairs, p)
+	}
+	return ans
+}
+
+func (i *input) inBounds(p *position) bool {
+	return p.row <= i.rows && p.row >= 0 && p.col <= i.cols && p.col >= 0
+}
+
+const EMPTY_SPACE byte = '.'
+
+// returns a row, col difference between 2 positions
+func distance(p1, p2 position) (int, int) {
+	return p2.row - p1.row, p2.col - p1.col
+}
+
+func (i *input) String() string {
+	var b strings.Builder
+	for r := range i.grid {
+		for c := range i.grid[r] {
+			b.WriteString(string(i.grid[r][c]))
+		}
+		b.WriteString("\n")
+	}
+	return b.String()
+}
+
+func main() {
+	f, _ := os.Open("aoc-day8-input.txt")
+	defer f.Close()
+	i := newInput(f)
+	i.computePart1()
+	fmt.Printf("Day 8 Part 1 ans=%d\n", len(i.antinodePositions))
+	// fmt.Println(i.String())
+}
---
+++ day8/main_test.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+)
+
+var _ = fmt.Print
+
+func Test_main(t *testing.T) {
+	tests := []struct {
+		name                            string
+		input                           string
+		expectedUniqueAntinodePositions int
+	}{
+		{
+			name: "web case",
+			input: `............
+        ........0...
+        .....0......
+        .......0....
+        ....0.......
+        ......A.....
+        ............
+        ............
+        ........A...
+        .........A..
+        ............
+        ............`,
+
+			expectedUniqueAntinodePositions: 14,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := strings.NewReader(tt.input)
+			i := newInput(r)
+
+			i.computePart1()
+
+			// fmt.Println(i.String())
+
+			// for k := range i.antinodePositions {
+			// 	i.grid[k.row][k.col] = 'X'
+			// }
+
+			// fmt.Println(i.String())
+			if len(i.antinodePositions) != tt.expectedUniqueAntinodePositions {
+				t.Errorf("wrong ap count,got=%v want=%v", len(i.antinodePositions), tt.expectedUniqueAntinodePositions)
+			}
+		})
+	}
+}
+
+func Test_distance(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+		p1    position
+		p2    position
+		want  int
+		want2 int
+	}{
+		// TODO: Add test cases.
+		{
+			name:  "t1",
+			p1:    position{row: 1, col: 2},
+			p2:    position{row: 1, col: 3},
+			want:  0,
+			want2: 1,
+		},
+		{
+			name:  "t2",
+			p1:    position{row: 1, col: 8},
+			p2:    position{row: 2, col: 5},
+			want:  1,
+			want2: -3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got2 := distance(tt.p1, tt.p2)
+			// TODO: update the condition below to compare got with tt.want.
+			if got != tt.want || got2 != tt.want2 {
+				t.Errorf("distance() = %v,%v, want %v,%v", got, got2, tt.want, tt.want2)
+			}
+		})
+	}
+}
+
+func BenchmarkP1(b *testing.B) {
+	f, _ := os.Open("aoc-day8-input.txt")
+	defer f.Close()
+	input := newInput(f)
+	for i := 0; i < b.N; i++ {
+		input.computePart1()
+	}
+}
